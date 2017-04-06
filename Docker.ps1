d422b76c295a13d8b55624f0a8a418c6a17b16d1
Koji Narazaki <koji@narachannel.com>
2017-04-06 23:49:48 +0900

    Windows Container testing

--- Docker.ps1
+++ Docker.ps1
@@ -1,3 +1,14 @@
-﻿docker ps -a
+﻿#Switch Linux Container to Windows Container (or Vise-versa)
+#& $Env:ProgramFiles\Docker\Docker\DockerCli.exe -SwitchDaemon
+
+docker ps -a
 
 docker images
+
+#Windows Container Hello world
+#https://docs.microsoft.com/en-us/virtualization/windowscontainers/quick-start/quick-start-windows-10
+docker pull microsoft/nanoserver
+
+docker run -it microsoft/nanoserver cmd
+
+powershell.exe Add-Content C:\helloworld.ps1 'Write-Host "Hello World"'
---
+++ InvokeMsBuild.ps1
@@ -0,0 +1,9 @@
+﻿function Invoke-MSBuild ([string]$MSBuildPath, [string]$MSBuildParameters) {
+    Invoke-Expression "$MSBuildPath $MSBuildParameters"
+}
+
+
+$msBuild = 'C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\'
+$buildParam = '.\ConsoleRandomAnswerGenerator.csproj /p:OutputPath=.\publish /p:Configuration=Release'
+
+Invoke-MSBuild -MSBuildPath $msBuild + "MSBuild.exe" -MSBuildParameters $buildParam
