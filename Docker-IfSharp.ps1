539c5fcaf442bef91274330921fe0417bdd720c8
Koji Narazaki <koji@narachannel.com>
2017-05-19 22:28:50 +0900

    Pushing Docker image example added

--- Docker-IfSharp.ps1
+++ Docker-IfSharp.ps1
@@ -2,10 +2,16 @@
 cd C:\Users\deton\Development\docker\IfSharp
 docker build -t ifsharp:3.0.0-alpha .
 
-#Rename docker image
-#docker tag xxx ifsharp
+#Rename docker image to push it to docker hub (xxx is image id)
+#docker tag xxx narachannel/ifsharp
 
+#Log in to docker hub
 docker login
 
 #push to docker hub
-docker push 
+docker push narachannel/ifsharp
+
+docker run -i -d -v C:/Users/deton/Development/docker/IfSharp/notebook:/notebooks -p 8888:8888 narachannel/ifsharp:latest
+
+#Start container
+#docker start -i xxxx
