50f369ebe2c12bf9a2ed628fa95837e5fffaa6f4
Koji Narazaki <koji@narachannel.com>
2017-05-29 16:12:58 +0900

    Eventlog added

---
+++ Eventlog.ps1
@@ -0,0 +1,10 @@
+﻿# Get latest 100 events from Docker service on Application log
+Get-EventLog -LogName Application -Newest 100 -Source DockerService
+
+# Create eventlog entry
+$eventSource = 'PowerShellToolbox'
+New-EventLog -LogName Application -Source $eventSource
+Write-EventLog -LogName Application -Source $eventSource `
+                -Message 'Hello from PowerShell!' -EntryType Information `
+                -EventId 1
+
