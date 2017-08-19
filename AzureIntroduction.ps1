9419597b33050364fe59242e4d98e5b44373c16c
Koji Narazaki <koji@narachannel.com>
2017-08-19 14:38:02 +0900

    Add Azure intoduction script

---
+++ AzureIntroduction.ps1
@@ -0,0 +1,16 @@
+﻿# Install Azure Modules
+# See https://docs.microsoft.com/en-us/powershell/azure/install-azurerm-ps?view=azurermps-4.2.0
+
+Get-Module PowerShellGet -list | Select-Object Name,Version,Path
+
+Import-Module AzureRM
+
+# If result of a command above does not show up, execute the command below to install modules
+Install-Module AzureRM -AllowClobber
+
+# Login to Azure
+Login-AzureRmAccount
+
+# Resource Group
+Get-AzureRmResourceGroup
+New-AzureRmResourceGroup -Name 'ResourceGroupName' -Location 'japaneast'
