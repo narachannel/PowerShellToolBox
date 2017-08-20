14e6f6198da2339191d206d7337aacfece7afb9d
Koji Narazaki <koji@narachannel.com>
2017-08-20 15:08:27 +0900

    Add SQLServer introduction

---
+++ SqlServerIntroduction.ps1
@@ -0,0 +1,20 @@
+﻿# Install SqlServer Modules
+Install-module -Name SqlServer -Scope CurrentUser
+
+# Import the SQL Server Module.    
+Import-Module SqlServer -DisableNameChecking
+
+# To check whether the module is installed.
+Get-Module -ListAvailable -Name SqlServer
+
+# Get database list from localhost
+Get-SqlDatabase -ServerInstance "(localdb)\MSSQLLocalDB"
+
+# Backup database
+$serverName = "(localdb)\MSSQLLocalDB"
+Backup-SqlDatabase -ServerInstance $serverName -Database 'Trading' -BackupFile '.\Development\sqlserver\trade.bak'
+
+# Restore database
+Restore-SqlDatabase -ServerInstance $serverName -BackupFile '.\Downloads\WideWorldImporters-Full.bak' `
+                    -DatabaseFile '.\Development\sqlserver\wwi.mdf' -Database 'wwifull'
+
