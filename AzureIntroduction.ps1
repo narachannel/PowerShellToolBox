fcb8953f911206abbc50c2c4652cf724b230b841
Koji Narazaki <koji@narachannel.com>
2017-09-05 23:30:58 +0900

    Add AzureRM Extension to reset password and SSH key

--- AzureIntroduction.ps1
+++ AzureIntroduction.ps1
@@ -14,3 +14,36 @@ Login-AzureRmAccount
 # Resource Group
 Get-AzureRmResourceGroup
 New-AzureRmResourceGroup -Name 'ResourceGroupName' -Location 'japaneast'
+
+# Get resource information (e.g. DocumentDB)
+Get-AzureRmResource -ResourceType "Microsoft/databaseAccounts" -ResourceGroupName "RGNAME" -Name "DocumentName" 
+
+# Create new virtual machine using Template and parameter json files downloaded from Azure
+$ResourceGroupName = 'ResourceName'
+$Location = 'japaneast'
+$templateFilePath = 'PATH\TO\TEMPLATE'
+$parametersFilePath = 'PATH\TO\PARAMETER'
+
+New-AzureRmResourceGroupDeployment -ResourceGroupName $resourceGroupName `
+    -TemplateFile $templateFilePath -TemplateParameterFile $parametersFilePath
+
+# Reset password and disable SSh
+$RGName = 'TestResources'
+$VmName = 'TestVM'
+$Location = 'eastus'
+
+$ExtensionName = 'VMAccessForLinux'
+$Publisher = 'Microsoft.OSTCExtensions'
+$Version = '1.4'
+
+$PublicConf = '{}'
+$PrivateConf = '{
+  "username": "testuser",  
+  "password": "newpassword",
+  "reset_ssh": true
+}'
+
+Set-AzureRmVMExtension -ResourceGroupName $RGName -VMName $VmName -Location $Location `
+  -Name $ExtensionName -Publisher $Publisher -ExtensionType $ExtensionName `
+  -TypeHandlerVersion $Version `
+  -Settingstring $PublicConf -ProtectedSettingString $PrivateConf
